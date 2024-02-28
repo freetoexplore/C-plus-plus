28ddf164e4502b9da58bfdc07957bedd033d5d4c
freetoexplore <116438028+freetoexplore@users.noreply.github.com>
2024-02-28 13:54:39 +0800

    Add files via upload

---
+++ 61 C++指针和数组.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+#include <string>
+
+int main() {
+
+	int arr1[] = { 1,2,3, 4,5,6 };
+	int* p = arr1;
+
+	int n = sizeof(arr1) / sizeof(arr1[0]);
+	int i = 0;
+	for ( i < 0; i < n; i++)
+	{
+		cout<< * p << endl;
+		p++; //指针循环
+	}
+
+	system("pause");
+	return 61;
+}
