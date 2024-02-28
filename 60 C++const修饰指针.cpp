77ccd988965ec7f7210e4250645ffb733f93eb89
freetoexplore <116438028+freetoexplore@users.noreply.github.com>
2024-02-28 13:37:54 +0800

    Add files via upload

---
+++ 60 C++const修饰指针.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+#include <string>
+
+int main() {
+
+	int a = 10;
+	int b = 10;
+
+	//常量指针，可以修改指针指向；不可修改指针值(p = 20;)
+	int * p = &a;
+	p = &b;
+
+	system("pause");
+	return 60;
+}
