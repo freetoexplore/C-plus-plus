e2be675674ef19aea06306f4a1757e66f485480d
freetoexplore <116438028+freetoexplore@users.noreply.github.com>
2024-02-28 14:43:33 +0800

    Add files via upload

---
+++ 62 C++指针和函数.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+#include <string>
+
+//值传递
+void swap(int a = 1, int b = 2)
+{
+	int temp = 0;
+	temp = a;
+	a = b;
+	b = temp;
+	cout << a << b << temp << endl;
+}
+
+//指针函数
+void swap2(int *p1, int *p2)
+{
+	int temp = 0;
+	temp = *p1;
+	*p1 = *p2;
+	*p2 = temp;
+	cout << *p1 << *p2 << temp << endl;
+}
+
+//投入变量a, b
+int main62() {
+
+	int a = 10;
+	int b = 20;
+	swap2(&a, &b);//地址传递，可修饰实际参数
+	return 62;
+
+}
---
+++ 63 C++指针数组和函数.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+#include <string>
+
+				//数值首地址（和原数组功能一样）
+void bubbleSort(int* arr, int len)
+{
+	cout << "Original Array: " << endl;
+	for (int o = 0; o< len;o++)
+	{
+		cout << arr[o] << endl;
+	}
+
+
+	for (int i = 0; i < len; i++)
+	{
+		for (int k = 0; k < len - 1 - i; k++)
+		{
+			if (arr[k] > arr[k + 1])
+			{
+
+				int temp = 0;
+				temp = arr[k+1];
+				arr[k + 1] = arr[k];
+				arr[k] = temp;
+			}
+		}
+	}
+
+	cout << "Sorted Array: " << endl;
+	for (int j = 0; j < len; j++)
+	{
+		cout << arr[j] << endl;
+	}
+}
+
+
+int main() {
+
+	int arr[] = {1, 3, 4, 2, 7};
+	int len = sizeof(arr) / sizeof(arr[0]);
+	bubbleSort(arr, len);
+
+	system("pause");
+	return 63;
+
+}
